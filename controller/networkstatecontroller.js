44a3086607cd1dd93f1506ca9611f3eeb2a92b78
chat-wane <grumpy.chat.wane@gmail.com>
2015-01-30 16:09:54 +0100

    disable unhandled functionnalities

--- controller/networkstatecontroller.js
+++ controller/networkstatecontroller.js
@@ -5,16 +5,25 @@
  * \brief model the model
  * \brief span the text that will show the connection state
  */
-function NetworkStateController(model,span){
+function NetworkStateController(model,span,abbr){
     var red = "#cd2626";
     var yellow = "#eead0e";
     var green = "#228b22";
 
     model.network._membership.on("statechange", function(state){
         switch (state){
-        case "connect": span.css("color", green); break;
-        case "partial": span.css("color", yellow); break;
-        case "disconnect": span.css("color", red); break;
+        case "connect":
+            span.css("color", green);
+            abbr.attr("title", "connected");
+            break;
+        case "partial":
+            span.css("color", yellow);
+            abbr.attr("title", "partially connected");
+            break;
+        case "disconnect":
+            span.css("color", red);
+            abbr.attr("title", "disconnected");
+            break;
         }
     });
     
