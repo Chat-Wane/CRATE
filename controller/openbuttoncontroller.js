ea71ee032b7acbc4b2161f84b5f48c0a4fe00246
chat-wane <grumpy.chat.wane@gmail.com>
2015-02-02 18:01:27 +0100

    first step to save open document

---
+++ controller/openbuttoncontroller.js
@@ -0,0 +1,8 @@
+
+function OpenButtonController(m, fileOpener){
+    $(":file").filestyle({input: false});
+    $("#open").css("background", "transparent");
+    $("#open").css("text-decoration", "none");
+    $("#open").css("cursor", "pointer");
+    $("#open").css("border", "none");
+};
