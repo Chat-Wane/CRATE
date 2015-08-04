0e73bab65dfdce150f6222158e270eeed6647599
chat-wane <grumpy.chat.wane@gmail.com>
2015-08-04 18:22:11 +0200

    hover button

--- js/view/roundbutton.js
+++ js/view/roundbutton.js
@@ -7,8 +7,15 @@ function RoundButton(container, text){
         .css('border-radius', '15px')
         .css('background', 'inherit')
         .css('padding', '6px 0')
-        .css('color', 'white')
+        .css('color', '#ececec')
         .css('vertical-align', 'middle')
         .html(text)
-        .prop('disable', true);
+        .prop('disable', true)
+        .hover(function(){
+            $(this).css('background-color', '#ececec');
+            $(this).css('color', 'black');
+        },     function(){
+            $(this).css('background-color', 'inherit');
+            $(this).css('color', '#ececec');
+        });
 };
