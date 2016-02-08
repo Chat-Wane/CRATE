f6621431d0f1670e6e86f251d642abe5e15f458a
chat-wane <grumpy.chat.wane@gmail.com>
2016-02-08 17:08:01 +0100

    about explanation of how it works

--- js/view/layers.js
+++ js/view/layers.js
@@ -18,6 +18,24 @@ function Layers(container){
                  {f: 2, t: 3},
                  {f: 2, t: 4},
                  {f: 3, t: 4}];
+
+    var explanations = [
+        {title: 'Editing session',
+         explanation: 'An editing session comprising 5 CRATE editors '+
+         'connected to each other.'},
+        {title: 'GUI layer',
+         explanation: 'Users write in their document using the '+
+         '<a href="https://ace.c9.io/">ace code editor</a>. Each of their '+
+         'changes instantly modifies their view of the document.'},
+        {title: 'Distributed sequence layer',
+         explanation: 'The structure that stores the document in such manner '+
+         'that all replicas converge to identical state when they received '+
+         'the same set of operations.'},
+        {title: 'Causality tracking layer',
+         explanation: ''},
+        {title: 'Network layer',
+         explanation: ''}
+    ];
     
     this.container = container;
 
@@ -29,7 +47,7 @@ function Layers(container){
         .css('border-top', 'dashed #ececec');
     
     var layers = [];
-    for (var i = 0; i<1; ++i){
+    for (var i = 0; i<5; ++i){
         // #A create the layers
         layers[i] =  jQuery('<div>').appendTo(rowLayers)
             .css('position', 'relative')
@@ -42,23 +60,67 @@ function Layers(container){
             .css('-o-transform', 'skew('+skew+'deg)')
             .addClass('post')
             .css('z-index', 5-i);
+
+        layers[i].popover({html: true,
+                           content: explanations[i].explanation,
+                           title: explanations[i].title,
+                           delay: {'hide':1000},
+                           container: 'body',
+                           trigger: 'hover'})
         // #B position points on each layer
         for (var j = 0; j < vertices.length; ++j){
-            switch (i){                
-            case 0: jQuery('<img>').appendTo(layers[i])
-                    .attr('src', './img/crateicon.png')
+            switch (i){
+            case 0: jQuery('<i>').appendTo(layers[i])
+                    .addClass('fa fa-cube')
                     .css('position', 'relative')
                     .css('left', -50+vertices[j].x+'%')
                     .css('top', -100+2*vertices[j].y+'%')
                     .css('-webkit-transform', 'skew('+(-skew)+'deg)')
                     .css('-moz-transform', 'skew('+(-skew)+'deg)')
                     .css('-o-transform', 'skew('+(-skew)+'deg)')
-                    .css('width','20px')
-                    .css('height', '20px')
                     .css('z-index', 5-i+0.5);
                 break;
+            case 1: jQuery('<i>').appendTo(layers[i])
+                    .addClass('fa fa-cloud')
+                    .css('position', 'relative')
+                    .css('left', -50+vertices[j].x+'%')
+                    .css('top', -100+2*vertices[j].y+'%')
+                    .css('-webkit-transform', 'skew('+(-skew)+'deg)')
+                    .css('-moz-transform', 'skew('+(-skew)+'deg)')
+                    .css('-o-transform', 'skew('+(-skew)+'deg)')
+                    .css('z-index', 4-i+0.5);
+                break;
+            case 2: jQuery('<i>').appendTo(layers[i])
+                    .addClass('fa fa-file-text')
+                    .css('position', 'relative')
+                    .css('left', -50+vertices[j].x+'%')
+                    .css('top', -100+2*vertices[j].y+'%')
+                    .css('-webkit-transform', 'skew('+(-skew)+'deg)')
+                    .css('-moz-transform', 'skew('+(-skew)+'deg)')
+                    .css('-o-transform', 'skew('+(-skew)+'deg)')
+                    .css('z-index', 3-i+0.5);
+                break;
+            case 3: jQuery('<i>').appendTo(layers[i])
+                    .addClass('fa fa-bar-chart')
+                    .css('position', 'relative')
+                    .css('left', -50+vertices[j].x+'%')
+                    .css('top', -100+2*vertices[j].y+'%')
+                    .css('-webkit-transform', 'skew('+(-skew)+'deg)')
+                    .css('-moz-transform', 'skew('+(-skew)+'deg)')
+                    .css('-o-transform', 'skew('+(-skew)+'deg)')
+                    .css('z-index', 2-i+0.5);
+                break;
+            case 4: jQuery('<i>').appendTo(layers[i])
+                    .addClass('fa fa-rocket')
+                    .css('position', 'relative')
+                    .css('left', -50+vertices[j].x+'%')
+                    .css('top', -100+2*vertices[j].y+'%')
+                    .css('-webkit-transform', 'skew('+(-skew)+'deg)')
+                    .css('-moz-transform', 'skew('+(-skew)+'deg)')
+                    .css('-o-transform', 'skew('+(-skew)+'deg)')
+                    .css('z-index', 1-i+0.5);
+                break;                
             };
         };
     };
 };
-
